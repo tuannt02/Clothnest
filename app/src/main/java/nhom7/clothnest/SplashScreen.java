ea03aba1cc794414353ca18d1b66e8cf6942f320
tuan-hda <anhtuan.hoangdinh@gmail.com>
2022-04-15 18:54:30 +0700

    Add Splash Screen

---
+++ app/src/main/java/nhom7/clothnest/SplashScreen.java
@@ -0,0 +1,15 @@
+package nhom7.clothnest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
