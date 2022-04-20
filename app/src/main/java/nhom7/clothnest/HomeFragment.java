f261c044a40fa0862518d43f89b85fbdaad93d14
VoDinhVan <20522147@gm.uit.edu.vn>
2022-04-20 22:07:16 +0700

    homepage

--- app/src/main/java/nhom7/clothnest/HomeFragment.java
+++ app/src/main/java/nhom7/clothnest/HomeFragment.java
@@ -7,58 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    GridView gridView;
+    ArrayList<Product> productArrayList;
+    GridViewApdater gridViewApdater;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v= inflater.inflate(R.layout.fragment_home, container, false);
+        gridView = v.findViewById(R.id.gridview);
+        GetProduct();
+        return v;
     }
+
+    public void GetProduct(){
+        productArrayList = new ArrayList<>();
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        productArrayList.add(new Product("Oversize Hoodie", R.drawable.productimage, "$307", "-24%"));
+        gridViewApdater = new GridViewApdater(getContext(), R.layout.thumbnail, productArrayList);
+        gridView.setAdapter(gridViewApdater);
+
+    }
+
 }
