7b74df89ed48b3b14f13973e0fbdc137c296d208
Phan Thanh Tú <pttu2902@gmail.com>
2022-05-14 21:06:29 +0700

    delete filter brand

--- app/src/main/java/nhom7/clothnest/fragments/FilterFragment.java
+++ app/src/main/java/nhom7/clothnest/fragments/FilterFragment.java
@@ -8,24 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.SeekBar;
 
 import java.util.ArrayList;
 
 import nhom7.clothnest.R;
-import nhom7.clothnest.adapters.Filter_BrandAdapter;
 import nhom7.clothnest.models.Filter_CheckItem;
 
 public class FilterFragment extends Fragment {
     private View mView;
     //view
-    private ListView lvBrand;
     private GridView gvSize, gvSizeInches, gvSizeCentimeters, gvColor;
     private SeekBar seekBar;
     //Brand
     private ArrayList<Filter_CheckItem> brandList;
-    private Filter_BrandAdapter brandAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,12 +37,10 @@ public class FilterFragment extends Fragment {
 
     private void getBrands() {
         brandList = new ArrayList<>();
-        brandAdapter = new Filter_BrandAdapter(mView.getContext(), brandList);
 //        Filter_BrandAdapter.getBrandAndNotify(brandList, brandAdapter);
     }
 
     private void reference() {
-        lvBrand = mView.findViewById(R.id.filter_BrandListView);
         gvSize = mView.findViewById(R.id.filter_sizeGridView);
         gvSizeInches = mView.findViewById(R.id.filter_sizeInchesGridView);
         gvSizeCentimeters = mView.findViewById(R.id.filter_sizeCentimeterGridView);
