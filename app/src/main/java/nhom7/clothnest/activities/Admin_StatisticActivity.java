26355c2265b9dbc5e508bce83eb1d8d33ef3e738
Tuan <20522122@gm.uit.edu.vn>
2022-05-20 11:18:27 +0700

    finished statistic + statistic detail
    connect db

--- app/src/main/java/nhom7/clothnest/activities/Admin_StatisticActivity.java
+++ app/src/main/java/nhom7/clothnest/activities/Admin_StatisticActivity.java
@@ -7,13 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import nhom7.clothnest.R;
+import nhom7.clothnest.models.Transaction;
+import nhom7.clothnest.models.User;
+import nhom7.clothnest.util.customizeComponent.CustomDialog;
+import nhom7.clothnest.util.customizeComponent.CustomProgressBar;
 
 public class Admin_StatisticActivity extends AppCompatActivity {
 
     ImageView btnClose;
     Button navViewDetail;
+    TextView txtRevenue;
+    TextView txtTotalTransaction;
+    TextView txtTransactionFinished;
+    TextView txtTransactionInProgress;
+    TextView txtTransactionCanceled;
+    TextView txtTotalUser;
+    TextView txtTotalProduct;
+    CustomProgressBar progressBar;
 
 
     @Override
@@ -26,11 +44,22 @@ public class Admin_StatisticActivity extends AppCompatActivity {
 
         // Set on click
         setOnClick();
+
+        // Set view
+        statistics();
     }
 
     private void initUi()   {
         btnClose = findViewById(R.id.admin_statistic_btn_close);
         navViewDetail = findViewById(R.id.admin_statistic_btn_view_detail);
+        txtRevenue = findViewById(R.id.admin_statistic_txt_revenue);
+        txtTotalTransaction = findViewById(R.id.admin_statistic_transaction);
+        txtTransactionFinished = findViewById(R.id.admin_statistic_txt_quantity_finished);
+        txtTransactionInProgress = findViewById(R.id.admin_statistic_txt_quantity_in_progress);
+        txtTransactionCanceled = findViewById(R.id.admin_statistic_txt_quantity_cancel);
+        txtTotalUser = findViewById(R.id.admin_statistic_txt_quantity_users);
+        txtTotalProduct = findViewById(R.id.admin_statistic_txt_quantity_products);
+        progressBar = new CustomProgressBar(Admin_StatisticActivity.this);
     }
 
     private void setOnClick()   {
@@ -49,5 +78,86 @@ public class Admin_StatisticActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void statistics()   {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        progressBar.show();
+        db.collection(User.COLLECTION_NAME)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int count = 0;
+                        for(DocumentSnapshot document : queryDocumentSnapshots) {
+                            count++;
+                        }
+                        txtTotalUser.setText(Integer.toString(count));
+                    }
+                });
+
+        db.collection("products")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int count = 0;
+                        for(DocumentSnapshot document : queryDocumentSnapshots) {
+                            count++;
+                        }
+                        txtTotalProduct.setText(Integer.toString(count));
+                    }
+                });
+
+
+        db.collection("products")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int count = 0;
+                        for(DocumentSnapshot document : queryDocumentSnapshots) {
+                            count++;
+                        }
+                        txtTotalProduct.setText(Integer.toString(count));
+                    }
+                });
+
+        db.collection(Transaction.COLLECTION_TRANSACTION)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        int count = 0;
+                        double revenue = 0;
+                        int qtyFinished = 0, qtyInprogress = 0, qtyCanceled = 0;
+                        for(DocumentSnapshot document : queryDocumentSnapshots) {
+                            String status = document.getString("status");
+                            count++;
+                            if(status.equals("Finished"))   {
+                                double revenueItem = document.getDouble("total");
+                                revenue+=revenueItem;
+                                qtyFinished++;
+                            }
+
+                            if(status.equals("In Progress"))   {
+                                qtyInprogress++;
+                            }
+
+                            if(status.equals("Canceled"))   {
+                                qtyCanceled++;
+                            }
+                        }
+
+                        txtRevenue.setText("$ " + revenue);
+                        txtTotalTransaction.setText(Integer.toString(count));
+                        txtTransactionFinished.setText(Integer.toString(qtyFinished));
+                        txtTransactionInProgress.setText(Integer.toString(qtyInprogress));
+                        txtTransactionCanceled.setText(Integer.toString(qtyCanceled));
+                        progressBar.dismiss();
+
+                    }
+                });
     }
 }
